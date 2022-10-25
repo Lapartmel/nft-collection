93da20351bcc2cf178fae64506c89a07456b9a69
lapartmel <lapartmel@gmail.com>
2022-10-25 17:56:08 +0200

    first message

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xE10D2cF1E38c6b139b0520B8972343f3Caa064fE";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nftcollection-qvds.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
